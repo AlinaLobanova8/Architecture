c1bca4c683d9880f0b4bdb53287aa157980a877e
Alina <alobanova8888@gmail.com>
2023-08-24 14:48:50 +0300

    Homework 2

---
+++ homework_2/fabric/Creator.java
@@ -0,0 +1,7 @@
+package Architecture.homework_2.fabric;
+
+abstract class Creator {
+
+    public abstract Product createProduct();
+    
+}
---
+++ homework_2/fabric/Pizza.java
@@ -0,0 +1,11 @@
+package Architecture.homework_2.fabric;
+
+public class Pizza implements Product {
+
+    @Override
+    public void showProduct() {
+
+        System.out.println("Это пицца");
+            
+    }       
+}
